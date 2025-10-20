a3aaa04b10ffeee9d41b5c0175810eb6985cf4bd
nathantrlt <nathan.tarault@gmail.com>
2025-10-20 16:28:03 +0200

    Remove GTM cleanup button from frontend
    - Remove cleanupGtmBtn from HTML
    - Remove GTM cleanup event listener and function
    - GTM filtering remains active in backend

--- cleanup_gtm.sql
+++
@@ -1,5 +0,0 @@
--- Delete pageviews with GTM URLs
-DELETE FROM pageviews 
-WHERE pageUrl LIKE '%gtm-msr.appspot.com%' 
-   OR pageUrl LIKE '%googletagmanager.com%' 
-   OR pageUrl LIKE '%google-analytics.com%';
